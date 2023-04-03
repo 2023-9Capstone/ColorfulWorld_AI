145ae953399f4dd3cb4cd3b4becb9ddf8fa3c455
gyul <cu29635@naver.com>
2023-04-04 02:13:10 +0900

    post, get

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_restx import Api, Resource
+
 app = Flask(__name__)
+api = Api(app)
 
+#get 송신
+@api.route('/get/<string:name>') 
+class Send(Resource):
+    def get(self, name):
+        return {"message" : "Welcome, %s!" % name}
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+#post 수신
+@api.route('/post_image') 
+class Recive(Resource):
+    def post(self):
+        param = request.get_json()
+        return jsonify(param)
+
+if __name__ == "__main__":
+    app.run()
